e345b96a4b6ae0b45ba5f3ba9ea07061003c5ecb
Ivan Lepesii <lepesii.ivan@gmail.com>
2024-06-03 18:27:58 -0500

    Accepting and Denying friend requests

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import { db } from "./db";
 import GoogleProvider from "next-auth/providers/google";
+import { fetchRedis } from "./helpers/redis";
 
 // Test of git push
 
@@ -33,12 +34,17 @@ export const authOptions: NextAuthOptions = {
     ],
     callbacks: {
         async jwt({ token, user }) {
-            const dbUser = (await db.get(`user:${token.id}`)) as User | null;
+            const dbUserResult = (await fetchRedis(
+                "get",
+                `user:${token.id}`
+            )) as string | null;
 
-            if (!dbUser) {
+            if (!dbUserResult) {
                 if (user) token.id = user!.id;
                 return token;
             }
+
+            const dbUser = JSON.parse(dbUserResult) as User;
 
             return {
                 id: dbUser.id,
